4deca734d0fdcf44895471b25cc4c5047ec43c0d
Segebari <41949716+Segebari@users.noreply.github.com>
2024-03-05 20:46:58 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://9nynt.netlify.app/', // Add your base URL here
   plugins: [react()],
 })
