dd2e6d4ca3bbd1dba59b9d6aec7237bbae4c98c4
Segebari <41949716+Segebari@users.noreply.github.com>
2024-03-05 20:38:36 +0100

    update

--- src/components/Aboutpage/Ourprocess.jsx
+++ src/components/Aboutpage/Ourprocess.jsx
@@ -5,7 +5,10 @@ const Ourprocess = () => {
     <>
       <div className="text-center text-black">
         <p className="font-semibold uppercase">The Process</p>
-        <h3 className="text-2xl font-bold">Built for Business Success</h3>
+        <h3 className="text-3xl pt-3 font-bold">Built for Business Success</h3>
+        <h4 className="text-xl pt-2 font-medium">
+          Discover How 9nynt Shop Benefits Various Industries
+        </h4>
       </div>
     </>
   )
