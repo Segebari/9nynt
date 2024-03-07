77a21881188fa7a6d524d9105c2e8c0de3445272
Segebari <41949716+Segebari@users.noreply.github.com>
2024-03-07 06:17:35 +0100

    update

--- src/components/Aboutpage/Work.jsx
+++ src/components/Aboutpage/Work.jsx
@@ -4,7 +4,12 @@ const Work = () => {
   return (
     <div className="h-auto w-full flex flex-col items-center py-10">
       <h3 className="font-bold text-4xl pb-10">We Work With Founders Who;</h3>
-      <div className="h-96 bg-work w-full"> </div>
+      <div className="h-96 bg-work w-full rounded-lg"> </div>
+      <p className="text-xl text-center px-48 pt-10">
+        Whether you're aiming for market expansion, increased efficiency, or
+        enhanced customer engagement, 9nynt Shop collaborates with founders to
+        achieve their unique business objectives.
+      </p>
     </div>
   )
 }
