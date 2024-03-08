73b067290343cbf33dfde55f2a8ebbcaa2af4383
Segebari <41949716+Segebari@users.noreply.github.com>
2024-03-08 11:07:26 +0100

    update

--- src/components/Aboutpage/Payment.jsx
+++ src/components/Aboutpage/Payment.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import visa from '../../assets/visa.svg'
+import master from '../../assets/mastercard.svg'
+import pay from '../../assets/9nynt-pay.svg'
+import verve from '../../assets/verve.svg'
 
 const Payment = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <h3 className="text-4xl text-center">Payment Methods</h3>
-      <p className="text-center">images</p>
+      <div className="flex my-10 gap-3">
+        <img src={visa} alt="logo" className="h-10 w-30 mb-2" />
+        <img src={master} alt="logo" className="h-10 w-30 mb-2" />
+        <img src={pay} alt="logo" className="h-10 w-30 mb-2" />
+        <img src={verve} alt="logo" className="h-10 w-30 mb-2" />
+      </div>
       <p className="text-2xl text-gray-500 text-center">
         We accept Visa, Mastercard and Verve
       </p>
