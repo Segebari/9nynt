1d60e890e987bcdd99a2e0bc473007b8090fb90e
Segebari <41949716+Segebari@users.noreply.github.com>
2024-03-11 14:55:28 +0100

    update

--- src/components/Aboutpage/Takecontrol.jsx
+++ src/components/Aboutpage/Takecontrol.jsx
@@ -3,9 +3,9 @@ import iphonea from '../../assets/iphonea.svg'
 import Downloadbuttons from '../Downloadbuttons'
 const Takecontrol = () => {
   return (
-    <div className="bg-down flex justify-between h-96 w-full my-28 rounded-md relative pt-14 px-10 overflow-hidden">
-      <div className="px-10">
-        <h2 className="text-white text-5xl font-bold">
+    <div className="bg-down flex justify-between h-96 w-full my-28 rounded-md relative pt-14 px-10 overflow-hidden xs:text-center lg:text-left">
+      <div className="xs:px-auto lg:px-10">
+        <h2 className="text-white xs:text-4xl lg:text-5xl font-bold">
           Take Control with 9nynt Shop!
         </h2>
         <p className="text-white pt-5 text-xl">
@@ -13,7 +13,7 @@ const Takecontrol = () => {
         </p>
         <Downloadbuttons />
       </div>
-      <div className="w-[50%] h-auto relative grid justify-items-end">
+      <div className="w-[50%] h-auto relative grid justify-items-end xs:hidden lg:block">
         <img src={iphonea} alt="Iphone" className=" w-[100%]" />
       </div>
     </div>
