76b2a9463dad0487e94a93108352df5474905bb6
Segebari <41949716+Segebari@users.noreply.github.com>
2024-03-11 14:37:53 +0100

    up

--- src/components/Aboutpage/Benefits/Benefits.jsx
+++ src/components/Aboutpage/Benefits/Benefits.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Benefits = ({ num, title, description }) => {
   return (
-    <div className="mt-10 py-10 bg-gray-100 w-[27%] px-10 rounded-2xl">
+    <div className="mt-10 py-10 bg-gray-100 xs:w-full md:w-[27%] px-10 rounded-2xl">
       <p className="bg-black text-white w-7 grid justify-center rounded text-sm py-[0.2rem] mb-5">
         {num}
       </p>
