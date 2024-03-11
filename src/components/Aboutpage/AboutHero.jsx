d65800b321b42ddf798a81e1c349689de6dbbfb2
Segebari <41949716+Segebari@users.noreply.github.com>
2024-03-11 11:41:46 +0100

    update

--- src/components/Aboutpage/AboutHero.jsx
+++ src/components/Aboutpage/AboutHero.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const AboutHero = () => {
   return (
     <>
-      <div className="w-full h-auto mt-[1rem] text-center px-52 pb-20 bg-gray-50 py-20 mb-8">
-        <h1 className=" text-6xl font-extrabold">
+      <div className="w-full h-auto mt-[1rem] text-center xs:px-auto md:px-52 pb-20 bg-gray-50 py-20 mb-8">
+        <h1 className=" xs:text-4xl md:text-6xl font-extrabold">
           <span className="text-gradient">Empower Your Business with </span>
           <span className="text-black border-b-8 border-black pb-1">9nynt</span>
           <span className="text-gradient pl-2">Shop</span>
